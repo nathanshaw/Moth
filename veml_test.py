063d1909614fd935d36a01c2a0bef432e9d5f6c1
Nathan Villicana-Shaw <nathanshawsemail@gmail.com>
2019-09-05 17:59:47 +1200

    fixed bug in veml test

--- veml_test.py
+++ veml_test.py
@@ -7,6 +7,6 @@ i2c = busio.I2C(board.SCL, board.SDA)
 veml = adafruit_veml7700.VEML7700(i2c)
 
 while(True):
-    print("ambiant light: ", veml7700.light)
-    print("ux:", veml7700.lux)
+    print("ambiant light: ", veml.light)
+    print("ux:", veml.lux)
     time.sleep(0.5)
